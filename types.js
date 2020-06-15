faba807da6b4ad30473b465eba8e56131f15b045
olympia <owojcik7@gmail.com>
2020-06-15 12:34:39 -0400

    Complete definition for Array type

--- types.js
+++ types.js
@@ -2,8 +2,19 @@ var myString;
 var myNum;
 var myBool;
 var myVar;
+/*
+var strArr: string[];
+var numArr: number[];
+var boolArr: boolean[];
+*/
+var strArr;
+var numArr;
+var boolArr;
 myString = 'Hello World';
 myNum = 222;
 myBool = true;
 myVar = 'Hello';
-console.log(myVar);
+strArr = ["Hello", "World"];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+console.log(boolArr);
--- types.ts
+++ types.ts
@@ -3,9 +3,24 @@ var myNum: number;
 var myBool: boolean;
 var myVar: any;
 
+/*
+var strArr: string[];
+var numArr: number[];
+var boolArr: boolean[];
+*/
+
+var strArr: Array<string>
+var numArr: Array<number>
+var boolArr: Array<boolean>
+
+
 myString = 'Hello World'
 myNum = 222
 myBool = true
 myVar = 'Hello'
 
-console.log(myVar)
+strArr = ["Hello", "World"]
+numArr = [1, 2, 3]
+boolArr = [true, false, true]
+
+console.log(boolArr)
