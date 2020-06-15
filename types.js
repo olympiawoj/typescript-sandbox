2e57d6b1b52631ff2483b3d33acb409d437309d6
olympia <owojcik7@gmail.com>
2020-06-15 12:27:10 -0400

    Complete variable definitions for String, Number, Boolean, and Any types

--- types.js
+++ types.js
@@ -1,1 +1,9 @@
-console.log('hello world!');
+var myString;
+var myNum;
+var myBool;
+var myVar;
+myString = 'Hello World';
+myNum = 222;
+myBool = true;
+myVar = 'Hello';
+console.log(myVar);
--- types.ts
+++ types.ts
@@ -1,1 +1,11 @@
-console.log('hello world!')
+var myString: string
+var myNum: number;
+var myBool: boolean;
+var myVar: any;
+
+myString = 'Hello World'
+myNum = 222
+myBool = true
+myVar = 'Hello'
+
+console.log(myVar)
