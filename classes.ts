afa3a5a5edde5fe3bbb8509561accc57373c1f6e
olympia <owojcik7@gmail.com>
2020-06-17 17:08:18 -0400

    Implement User/Member class inheritance

--- classes.ts
+++ classes.ts
@@ -17,9 +17,33 @@ class User {
     register() {
         console.log(this.name + ' is now registered')
     }
+
+    payInvoice() {
+        console.log(this.name + ' paid invoice.')
+    }
 }
 
 
-let john = new User('John', "jdoe@gmail.com", 34)
+// extend means it's inheriting from the User class
+class Member extends User {
+    id: number;
 
-console.log(john.register())
+    // paste property along with inherited ones
+    constructor(id: number, name: string, email: string, age: number) {
+        //super takes in properties class is inheriting
+        super(name, email, age)
+        this.id = id
+    }
+    payInvoice() {
+        super.payInvoice()
+    }
+}
+
+// let john = new User('John', "jdoe@gmail.com", 34)
+
+// console.log(john.register())
+
+// Mike Smith is a user, but he's also a member so let's call payInvoice
+let mike = new Member(1, 'Mike Smith', 'mike@gmail.com', 33)
+
+mike.payInvoice()
